0efc8edec7efa6a6fa4a4fa43595abce40140e61
alifionauval <147744181+alifionauval@users.noreply.github.com>
2025-01-19 11:52:42 +0700

    Update TentangKami.js

--- src/pages/TentangKami.js
+++ src/pages/TentangKami.js
@@ -849,9 +849,6 @@ const TentangKami = () => {
       <section className="cta-section">
         <h3 className="cta-text">Give Some Snaps To</h3>
         <h4 className="cta-text">Your Brand !</h4>
-        <Link to="/kontak-kami">
-          <button className="cta-button">Contact Us !</button>
-        </Link>
       </section>
     </div>
   );
